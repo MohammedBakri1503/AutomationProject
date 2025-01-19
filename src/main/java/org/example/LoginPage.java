9203da74b82b02bc2b9f53b72e079f3f319cc807
MohammedBakri1503 <mohammedbakri1503@gmail.com>
2025-01-19 18:43:24 +0200

    update API yaml

--- src/main/java/org/example/LoginPage.java
+++ src/main/java/org/example/LoginPage.java
@@ -14,11 +14,12 @@ public class LoginPage {
     private By loginButtonBy = By.cssSelector("#pageContent > div.css-19pv48c > div > div > div > div.css-1f4iiuo > div > div > form > button");
     private By successMessageBy = By.id("toast-container");
     private By invalidlog = By.cssSelector("#pageContent > div.css-19pv48c > div > div > div > div.css-1f4iiuo > div > div.css-1du4g3n > div > div.css-vjkmk1 > div");
-
+    private By ENT = By.cssSelector("#root > div > main > div > div > section.mb-4.border.border-gray-300.bg-white.shadow-md > div > footer > button");
     public LoginPage(WebDriver driver) {
         this.driver = driver;
 
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        this.driver.findElement(ENT).click();
 
         if (!driver.getTitle().contains("Grafana")) {
             throw new IllegalStateException("This is not the Login Page. Current page: " + driver.getCurrentUrl());
